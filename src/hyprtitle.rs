14f120f1642279de802f5247847e0c34e4f44d9c
Yuto Liyosa <yutoli.pro@gmail.com>
2025-01-31 15:45:50 +0330

    add icons fullscreen and maximized

--- src/hyprtitle.rs
+++ src/hyprtitle.rs
@@ -12,6 +12,8 @@ const NORMAL_WINDOW_ICON: &str = " ";
 const XWAYLAND_WINDOW_ICON: &str = " ";
 const PINNED_WINDOW_ICON: &str = " ";
 const FLOATING_WINDOW_ICON: &str = "󰨦 ";
+const FULLSCREEN_WINDOW_ICON: &str = "󰊓 ";
+const MAXIMIZED_WINDOW_ICON: &str = " ";
 const WINDOW_CLASS_ICON: &str = " ";
 const WINDOW_SIZE_ICON: &str = "󰳂 ";
 const WINDOW_POSITION_ICON: &str = " ";
@@ -108,29 +110,35 @@ impl Hyprtitle {
         let mut grouped_windows = String::new();
 
         if let Some(active_window) = self.active_window.as_ref() {
-            if active_window.grouped.len() > 1 {
-                let index = active_window
-                    .grouped
-                    .iter()
-                    .position(|address| **address == active_window.address)
-                    .unwrap_or(0)
-                    + 1;
+            let grouped_window_count = active_window.grouped.len();
+            if grouped_window_count > 0 {
+                grouped_windows.push_str(format!(" {GROUPED_WINDOWS_ICON}").as_str());
 
-                grouped_windows = format!(
-                    " {GROUPED_WINDOWS_ICON}{}/{}",
-                    index,
-                    active_window.grouped.len()
-                )
+                if grouped_window_count > 1 {
+                    let index = active_window
+                        .grouped
+                        .iter()
+                        .position(|address| **address == active_window.address)
+                        .unwrap_or(0)
+                        + 1;
+
+                    grouped_windows
+                        .push_str(format!("{}/{}", index, grouped_window_count).as_str());
+                }
             }
 
             let mut title_icon = NORMAL_WINDOW_ICON;
 
-            if active_window.xwayland {
-                title_icon = XWAYLAND_WINDOW_ICON
-            } else if active_window.pinned {
+            if active_window.pinned {
                 title_icon = PINNED_WINDOW_ICON
             } else if active_window.floating {
                 title_icon = FLOATING_WINDOW_ICON
+            } else if active_window.fullscreen == FullscreenMode::Maximized {
+                title_icon = MAXIMIZED_WINDOW_ICON
+            } else if active_window.fullscreen_client == FullscreenMode::Fullscreen {
+                title_icon = FULLSCREEN_WINDOW_ICON
+            } else if active_window.xwayland {
+                title_icon = XWAYLAND_WINDOW_ICON
             }
 
             title = title_icon.to_string() + active_window.title.as_ref();
@@ -184,11 +192,18 @@ impl Hyprtitle {
             }};
         }
 
-        listener.add_window_closed_handler(window_handler!(hyprtitle));
-        listener.add_window_opened_handler(window_handler!(hyprtitle));
-        listener.add_window_moved_handler(window_handler!(hyprtitle));
-        listener.add_window_title_changed_handler(window_handler!(hyprtitle));
         listener.add_active_window_changed_handler(window_handler!(hyprtitle));
+        listener.add_float_state_changed_handler(window_handler!(hyprtitle));
+        listener.add_fullscreen_state_changed_handler(window_handler!(hyprtitle));
+        listener.add_group_toggled_handler(window_handler!(hyprtitle));
+        listener.add_window_closed_handler(window_handler!(hyprtitle));
+        listener.add_window_moved_handler(window_handler!(hyprtitle));
+        listener.add_window_moved_into_group_handler(window_handler!(hyprtitle));
+        listener.add_window_moved_out_of_group_handler(window_handler!(hyprtitle));
+        listener.add_window_opened_handler(window_handler!(hyprtitle));
+        listener.add_window_pinned_handler(window_handler!(hyprtitle));
+        listener.add_window_title_changed_handler(window_handler!(hyprtitle));
+
         hyprtitle.borrow_mut().update(None).print();
         listener.start_listener().unwrap();
     }
